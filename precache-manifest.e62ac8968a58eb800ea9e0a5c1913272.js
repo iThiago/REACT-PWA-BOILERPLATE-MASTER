8349d052d66bab3163ae92ff50ec301fcb9ba23e
Thiago Neto <thiagonetorj@hotmail.com>
2020-01-04 16:08:29 -0300

    Updates

---
+++ precache-manifest.e62ac8968a58eb800ea9e0a5c1913272.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d6b616a91fa1979f312bddac2fd1f6",
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/index.html"
+  },
+  {
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/precache-manifest.c342a33377fe11957832b557f84f8a06.js"
+  },
+  {
+    "revision": "d0e9f29bb21fe097decc106c0f36ade8",
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/service-worker.js"
+  },
+  {
+    "revision": "15cd4a59d40bfd78e183",
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/static/css/main.04cf1866.chunk.css"
+  },
+  {
+    "revision": "6fb39670c430fe48f0ab",
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/static/js/2.3e72ad30.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/static/js/2.3e72ad30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15cd4a59d40bfd78e183",
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/static/js/main.56bc031b.chunk.js"
+  },
+  {
+    "revision": "ae0e60ec4f4a3b50f64d",
+    "url": "/REACT-PWA-BOILERPLATE-MASTER/static/js/runtime-main.e054d7a4.js"
+  }
+]);
